fb1f61f35101ef319944a6049548b96c9a9ef0e9
opyon <opyon3@gmail.com>
2018-10-09 15:54:58 +0900

    no message

--- Python3/qa/qa150830_test02.py
+++ Python3/qa/qa150830_test02.py
@@ -1,60 +1,47 @@
 import timeit
 import math
 
-# def primesSieve(n):
-#     # 追加①
-#     n += 1
-#     sieve = [True] * (n)
-#     sieve[0] = sieve[1] = False
-#     for i in range(2, n):
-#         for j in range(i * i, n, i * 2):
-#             sieve[j] = False
-#
-#     return sieve
+'''
+#修正２
+#sieve_ans = [False] * n
+#sieve_ansリストを新規追加してTrue/Falseのみで素数を判定しておく
+#primesリストは初回取得時のみと毎回の区間篩の時に使うのみにした
+#最後にreturnでsieve_ansをリストに変換
+#速度は3秒程遅くなり約20秒前後
+'''
 
-# @swordoneさんのご指摘により
-# 上記def primesSieve(n):を丸ごと差し替え
+# 新：エラトステネスの篩：修正２
 def eratosthenes_sieve(n):
-    # 追加①より
     n += 1
-
     sieve = [True] * n
     sieve[0] = sieve[1] = False
 
-    #2の倍数の処理だけ最初に行う
     for i in range(4, n, 2):
         sieve[i] = False
 
-    #以降は奇数のみ処理
     for prime in range(3, n, 2):
         if sieve[prime]:
             for i in range(prime * prime, n, prime * 2):
                 sieve[i] = False
     return sieve
 
-# 新：区間篩
+# 新：区間篩：修正２
 def primesList(n):
-        # 追加②-1
         n -= 1
-
-        # 修正②-2
         SIEVE_MAX = int(math.sqrt(n)) + 1
 
         primes = []
-        # 修正②-3
-        # +丸ごと差し替え
-        # sieve = primesSieve(SIEVE_MAX - 1)
         sieve = eratosthenes_sieve(SIEVE_MAX - 1)
 
-# 中身確認用
-#         print([j for j in range(len(sieve)) if sieve[j]])
-
+        #修正２で追加
+        sieve_ans = [False] * (n + 1)
         for i in range(len(sieve)):
             if sieve[i]:
+
+                #修正２で追加
+                sieve_ans[i] = True
                 primes.append(i)
 
-        # 修正③
-        # for i in range(1, int(n / SIEVE_MAX)):
         for i in range(1, (n // SIEVE_MAX) + 1):
 
             sieve = [True] * len(sieve)
@@ -64,29 +51,23 @@ def primesList(n):
                 if (p * p > start_index + SIEVE_MAX):
                     break
 
-                # 修正④
-                # jj = start_index + (p - (start_index % p)) % p
-#                 print(jj, start_index + SIEVE_MAX, p)
-                # for j in range(jj, start_index + SIEVE_MAX, p):
-                    # sieve[j - start_index] = False
-
                 jj = (p - (start_index % p)) % p
+
                 for j in range(jj, SIEVE_MAX, p):
                     sieve[j] = False
+#                     print('jj:',jj,'start:',start_index,'end:',start_index + SIEVE_MAX,'p:', p,'j:',start_index +j)
 
-            # 修正⑤-1
-            # for j in range(0, len(sieve)):
             for j in range(min([n - start_index + 1, SIEVE_MAX])):
                 if sieve[j]:
 
-                    # 修正⑤-2
-                    # primes.append(start_index + j)
-                    primes.append(start_index + j)
+                        #修正２で追加
+                        sieve_ans[start_index+j] = True
 
-        return primes
+        #修正２で追加
+        return [i for i,p in enumerate(sieve_ans) if p]
 
 
-# 旧：エラトステネスの篩
+# 旧：エラトステネスの篩：比較用
 def eratosthenes_list(n):
     primes = [True] * n
     result = [2]
@@ -98,19 +79,20 @@ def eratosthenes_list(n):
     return result
 
 
-def test(nn):
-#     for nn in range(10, nn, nn // 10):
-#         a = eratosthenes_list(nn)
-#         b = primesList(nn)
-#         if not a == b:
-#             print('nn:', nn)
-#             print("miss")
-#             print(len(a), len(b))
-#             print(a)
-#             print(b)
-#             break
-#     else:
-#         print("all ok")
+def test(nn,test_on):
+    if test_on:
+        for nn in range(10, nn, nn // 10):
+            a = eratosthenes_list(nn)
+            b = primesList(nn)
+            if not a == b:
+                print('nn:', nn)
+                print("miss")
+                print(len(a), len(b))
+                print(a)
+                print(b)
+                break
+        else:
+            print("all ok")
 
 #     修正後テスト
     print('区間篩修正:',timeit.timeit(lambda: (primesList(nn)), number = 1))
@@ -124,20 +106,28 @@ def test(nn):
 
 
 nn = 10 ** 8
-test(nn)
+test(nn,0)
+# 新：区間篩：修正２
+# nn = 100000000
+# 区間篩修正: 19.98532157
 
 '''
 #以下過去ログ
 '''
-# 新
+# 新：区間篩：修正１
+# nn = 100000000
+# 区間篩修正: 17.563386085
+
+# 新：区間篩：質問時初期
 # nn = 100000000
 # 24.349327380000002
 
-# 旧
+# 旧：エラトステネスの篩：比較用
 # nn = 100000000
 # 11.725847288999997
 
 # test確認用
+
 # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 #                10 4個
 #              100 25個
--- Python3/qa/qa150830_test03.py
+++ Python3/qa/qa150830_test03.py
@@ -1,60 +1,36 @@
 import timeit
 import math
 
-# def primesSieve(n):
-#     # 追加①
-#     n += 1
-#     sieve = [True] * (n)
-#     sieve[0] = sieve[1] = False
-#     for i in range(2, n):
-#         for j in range(i * i, n, i * 2):
-#             sieve[j] = False
-#
-#     return sieve
+'''
+#最終清書
+'''
 
-# @swordoneさんのご指摘により
-# 上記def primesSieve(n):を丸ごと差し替え
+# 新：エラトステネスの篩：最終
 def eratosthenes_sieve(n):
-    # 追加①より
     n += 1
-
     sieve = [True] * n
     sieve[0] = sieve[1] = False
 
-    #2の倍数の処理だけ最初に行う
     for i in range(4, n, 2):
         sieve[i] = False
 
-    #以降は奇数のみ処理
     for prime in range(3, n, 2):
         if sieve[prime]:
             for i in range(prime * prime, n, prime * 2):
                 sieve[i] = False
     return sieve
 
-# 新：区間篩
+# 新：区間篩：最終
 def primesList(n):
-        # 追加②-1
         n -= 1
-
-        # 修正②-2
         SIEVE_MAX = int(math.sqrt(n)) + 1
-
         primes = []
-        # 修正②-3
-        # +丸ごと差し替え
-        # sieve = primesSieve(SIEVE_MAX - 1)
         sieve = eratosthenes_sieve(SIEVE_MAX - 1)
-
-# 中身確認用
-#         print([j for j in range(len(sieve)) if sieve[j]])
 
         for i in range(len(sieve)):
             if sieve[i]:
                 primes.append(i)
 
-        # 修正③
-        # for i in range(1, int(n / SIEVE_MAX)):
         for i in range(1, (n // SIEVE_MAX) + 1):
 
             sieve = [True] * len(sieve)
@@ -64,29 +40,20 @@ def primesList(n):
                 if (p * p > start_index + SIEVE_MAX):
                     break
 
-                # 修正④
-                # jj = start_index + (p - (start_index % p)) % p
-#                 print(jj, start_index + SIEVE_MAX, p)
-                # for j in range(jj, start_index + SIEVE_MAX, p):
-                    # sieve[j - start_index] = False
-
                 jj = (p - (start_index % p)) % p
+
                 for j in range(jj, SIEVE_MAX, p):
                     sieve[j] = False
+#                     print('jj:',jj,'start:',start_index,'end:',start_index + SIEVE_MAX,'p:', p,'j:',start_index +j)
 
-            # 修正⑤-1
-            # for j in range(0, len(sieve)):
             for j in range(min([n - start_index + 1, SIEVE_MAX])):
                 if sieve[j]:
-
-                    # 修正⑤-2
-                    # primes.append(start_index + j)
-                    primes.append(start_index + j)
+                    primes.append(start_index+j)
 
         return primes
 
 
-# 旧：エラトステネスの篩
+# 旧：エラトステネスの篩：比較用
 def eratosthenes_list(n):
     primes = [True] * n
     result = [2]
@@ -98,22 +65,23 @@ def eratosthenes_list(n):
     return result
 
 
-def test(nn):
-#     for nn in range(10, nn, nn // 10):
-#         a = eratosthenes_list(nn)
-#         b = primesList(nn)
-#         if not a == b:
-#             print('nn:', nn)
-#             print("miss")
-#             print(len(a), len(b))
-#             print(a)
-#             print(b)
-#             break
-#     else:
-#         print("all ok")
+def test(nn,test_on):
+    if test_on:
+        for nn in range(10, nn, nn // 10):
+            a = eratosthenes_list(nn)
+            b = primesList(nn)
+            if not a == b:
+                print('nn:', nn)
+                print("miss")
+                print(len(a), len(b))
+                print(a)
+                print(b)
+                break
+        else:
+            print("all ok")
 
 #     修正後テスト
-    print('区間篩修正:',timeit.timeit(lambda: (primesList(nn)), number = 1))
+#     print('区間篩修正:',timeit.timeit(lambda: (primesList(nn)), number = 1))
 #     print('旧篩　　　:',timeit.timeit(lambda: (eratosthenes_list(nn)), number = 1))
 
 #     print('区間篩修正:',timeit.timeit(lambda: print((primesList(nn))), number = 1))
@@ -123,21 +91,27 @@ def test(nn):
 #     print('旧篩　　　:',timeit.timeit(lambda: print(len(eratosthenes_list(nn))), number = 1))
 
 
-nn = 10 ** 8
-test(nn)
+nn = 10 ** 3
+# 区間篩修正: 0.0002253680000000001
+test(nn,0)
 
 '''
 #以下過去ログ
 '''
-# 新
+# 新：区間篩：修正１
+# nn = 100000000
+# 区間篩修正: 17.563386085
+
+# 新：区間篩：質問時初期
 # nn = 100000000
 # 24.349327380000002
 
-# 旧
+# 旧：エラトステネスの篩：比較用
 # nn = 100000000
 # 11.725847288999997
 
 # test確認用
+
 # [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 #                10 4個
 #              100 25個
